8a5ffe351f52ec9133bf2b241813b4548693c9e8
sweir27 <sweir27@gmail.com>
2016-05-10 15:25:20 -0400

    adding status to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
   def create
     puts 'Create called', params[:orders]
     @order = Order.new(params[:orders].permit(:name, :phone))
+    @order.update_attributes!(status: 'Pending')
     params[:orders][:oysters].each do |name, count|
       oyster = Oyster.where(name: name).first
       return unless oyster
@@ -13,21 +14,14 @@ class OrdersController < ApplicationController
     end
 
     if @order.save
-      redirect_to order_path(@order.id)
+      redirect_to order_path(activation_code: @order.activation_code, id: @order.id)
     else
       render 'new'
     end
   end
 
   def show
-    if params[:id]
-      @order = Order.find(params[:id])
-    elsif params[:activation_code]
-      @order = Order.where(activation_code: params[:activation_code]).first
-    else
-      flash[:error]
-      redirect_to root_path
-    end
+    @order = Order.where(activation_code: params[:activation_code]).first
     @oysters = []
     @order.orders_oysters.each do |order_oyster|
       return if order_oyster.count.blank?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510165425) do
+ActiveRecord::Schema.define(version: 20160510191724) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160510165425) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "activation_code"
+    t.string   "status"
   end
 
   create_table "orders_oysters", id: false, force: :cascade do |t|
