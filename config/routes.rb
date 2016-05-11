d95d0644fe52b17adfd54a331feecbff57550288
sweir27 <sweir27@gmail.com>
2016-05-11 14:16:48 -0400

    adds index view with ability to mark as done

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :oysters
-  resources :orders
+  resources :orders do
+    member do
+      post 'done'
+    end
+  end
 
   # Example resource route with options:
   #   resources :products do
